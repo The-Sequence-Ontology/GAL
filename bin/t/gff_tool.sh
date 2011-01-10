6a5e28de5994ef470d964361a6faa593fbc998ce
Barry Moore <barry.moore@genetics.utah.edu>
2011-01-10 01:15:37 +0000

    mods to gff_tool and tests
    Former-commit-id: 448d0d27bcd9d79baf9ed8c8f757391bce423f9e

--- bin/t/gff_tool.sh
+++ bin/t/gff_tool.sh
@@ -6,3 +6,27 @@
 ../gff_tool --out_ext .header --grab_headers 10Gen_Chinese_SNV_partial.gvf 
 ../gff_tool --add_headers 10Gen_Chinese_SNV_partial.gvf.header junk.gff 
 
+../gff_tool --filter
+../gff_tool --merge_gff
+../gff_tool --blend_gff
+../gff_tool --sort_gff
+../gff_tool --alter_gff
+../gff_tool --hash_ag
+../gff_tool --validate
+../gff_tool --stats
+../gff_tool --fasta_feature
+../gff_tool --fasta_add
+../gff_tool --fasta_only
+../gff_tool --fasta_no
+../gff_tool --add_ID
+../gff_tool --pragmas
+../gff_tool --headers_only
+../gff_tool --headers_no
+../gff_tool --meta_add
+../gff_tool --features
+../gff_tool --union
+../gff_tool --intersection
+../gff_tool --l_compliment
+../gff_tool --s_difference
+../gff_tool --calc_titv
+../gff_tool --get_so_data
