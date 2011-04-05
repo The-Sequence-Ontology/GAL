189f8c0b97b23d1fda230edcb43a76d52f9bdbe2
Barry Moore <barry.moore@genetics.utah.edu>
2011-04-05 21:58:02 +0000

    Fixing gff_tool validate as per Graham Richie e-mail 4/5/11
    Former-commit-id: 01fb5c121ca3a039cdf9dafd16f67e77eb55772b

---
+++ t/gvf_tool.t.sh
@@ -0,0 +1,1 @@
+../bin/gff_tool --validate data/Canis_familiaris_incl_consequences.gvf > data/Canis_familiaris_incl_consequences.validate.out 2> data/Canis_familiaris_incl_consequences.validate.error
