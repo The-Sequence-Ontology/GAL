0de041fcdff7b73fb28f1a8aa308d664d8cfff43
Barry Moore <barry.moore@genetics.utah.edu>
2012-11-02 23:30:41 +0000

    Minor update to cleanup.sh
    Former-commit-id: 2eb5382f4a111e3fabab9e364988f20e8f7e7cee

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,7 @@
 rm -f lib/GAL/t/data/dmel-4-r5.24.partial.sqlite
 rm -f lib/GAL/t/data/test_storage.sqlite
+rm -f t/data/dmel-4-r5.46.genes.sqlite
+rm -f t/data/10Gen_Korean_SNV_chr22.sqlite
+rm -f t/data/refseq_chr22.trim.sqlite
+rm -f t/data/directory.index
+rm -f t/data/fasta_hg18/chr22_hg18.fasta.index
